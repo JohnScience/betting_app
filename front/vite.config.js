6167dd5964d8f544c7d004d7062ce45a7b6f4653
Dmitrii Demenev <demenev.dmitriy1@gmail.com>
2023-09-17 16:07:01 -0600

    fix: Fixed build on Android and improved font size for mobile devices

--- front/vite.config.js
+++ front/vite.config.js
@@ -3,6 +3,11 @@ import wasm from "vite-plugin-wasm";
 import topLevelAwait from "vite-plugin-top-level-await";
 
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      external: ["cordova.js"]
+    }
+  },
   plugins: [
     wasm(),
     topLevelAwait()
